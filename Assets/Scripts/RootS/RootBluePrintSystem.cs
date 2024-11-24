d2179b5211139837a5d1fe708d23f756139876ee
Anigilator322 <Anigilator322@gmail.com>
2024-11-24 17:37:49 +0700

    Renaming

--- Assets/Scripts/RootS/RootBluePrintSystem.cs
+++ Assets/Scripts/RootS/RootBluePrintSystem.cs
@@ -15,7 +15,7 @@ namespace Assets.Scripts.RootS
         private float _clickedNodeSearchRadius = 2f;
         private float _distanceToBuildNewNode = 2f;
         private float _maxBuildAngle = 90f;
-        private RootBuildingPath _rootBuildingPath;
+        public RootBuildingPath RootBuildingPath;
         private bool isClickedOnRoot(Vector2 mousePos)
         {
             if (_gridPartition.Query(_clickedNodeSearchRadius, mousePos).Count != 0)
@@ -30,7 +30,6 @@ namespace Assets.Scripts.RootS
 
         private void Start()
         {
-            _playerInputActions.PlayerMap.Enable();
             _playerInputActions.PlayerMap.LBMPressed.performed += _ => 
             { 
                 if (isClickedOnRoot(_playerInputActions.PlayerMap.MousePosition.ReadValue<Vector2>()))
@@ -39,7 +38,7 @@ namespace Assets.Scripts.RootS
                     PrepareBLuePrint(_playerInputActions.PlayerMap.MousePosition.ReadValue<Vector2>()); 
                 } 
             };
-            _playerInputActions.PlayerMap.LBMPressed.canceled += _ => { _isDragging = false; };
+            _playerInputActions.PlayerMap.LBMPressed.canceled += _ => { _isDragging = false; CancelBluePrinting(); };
         }
 
         private void Update()
@@ -52,16 +51,16 @@ namespace Assets.Scripts.RootS
             List<RootNode> quiredNodes = _gridPartition.Query(_clickedNodeSearchRadius,mousePosition);
             RootNode _clickedNode;
             _clickedNode = FindClosestNodeToMouse(quiredNodes, mousePosition);
-            _rootBuildingPath = new RootBuildingPath();
+            RootBuildingPath = new RootBuildingPath();
             if(_clickedNode.nextNodes.Count == 0)
             {
-                _rootBuildingPath.IsNewProcess = false;
-                _rootBuildingPath.AddInPath(_clickedNode.prevNode.Position);
-                _rootBuildingPath.AddInPath(_clickedNode.Position);
+                RootBuildingPath.IsNewProcess = false;
+                RootBuildingPath.AddInPath(_clickedNode.prevNode.Position);
+                RootBuildingPath.AddInPath(_clickedNode.Position);
             }
             else
             {
-                _rootBuildingPath.IsNewProcess = true;
+                RootBuildingPath.IsNewProcess = true;
             }
         }
 
@@ -82,7 +81,7 @@ namespace Assets.Scripts.RootS
 
         private void CreateNewPathNode(Vector2 position)
         {
-            _rootBuildingPath.AddInPath(position);
+            RootBuildingPath.AddInPath(position);
         }
 
         private void TryBlueprint()
@@ -92,7 +91,7 @@ namespace Assets.Scripts.RootS
             Debug.Log("Dragging");
             Vector2 mousePos = _playerInputActions.PlayerMap.MousePosition.ReadValue<Vector2>();
 
-            if (Vector2.Distance(mousePos, _rootBuildingPath.RootPath[_rootBuildingPath.RootPath.Count-1]) > _distanceToBuildNewNode)
+            if (Vector2.Distance(mousePos, RootBuildingPath.RootPath[RootBuildingPath.RootPath.Count-1]) > _distanceToBuildNewNode)
             {
                 if(IsCreating(mousePos))
                 {
@@ -102,13 +101,13 @@ namespace Assets.Scripts.RootS
                 {
                     DecreasePath();
                 }
-                _rootBuildingPath.IsPathCorrect = CheckPathCorrection();
+                RootBuildingPath.IsPathCorrect = CheckPathCorrection();
             }
         }
 
         private bool CheckPathCorrection()
         {
-            int count = _rootBuildingPath.RootPath.Count;
+            int count = RootBuildingPath.RootPath.Count;
             if (count < 2)
             {
                 return true;
@@ -117,9 +116,9 @@ namespace Assets.Scripts.RootS
             for (int i = 2; i < count; i++)
             {
                 //Add rules
-                Vector2 P1 = _rootBuildingPath.RootPath[i-2];
-                Vector2 P2 = _rootBuildingPath.RootPath[i-1];
-                Vector2 P3 = _rootBuildingPath.RootPath[i];
+                Vector2 P1 = RootBuildingPath.RootPath[i-2];
+                Vector2 P2 = RootBuildingPath.RootPath[i-1];
+                Vector2 P3 = RootBuildingPath.RootPath[i];
 
 
                 float cosTheta = Vector3.Dot(P2-P1, P3-P2);
@@ -134,18 +133,18 @@ namespace Assets.Scripts.RootS
 
         private void DecreasePath()
         {
-            _rootBuildingPath.RootPath.RemoveAt(_rootBuildingPath.RootPath.Count-1);
+            RootBuildingPath.RootPath.RemoveAt(RootBuildingPath.RootPath.Count-1);
         }
 
         private void CreatePathNode(Vector2 mousePos)
         {
-            _rootBuildingPath.RootPath.Add(mousePos);
+            RootBuildingPath.RootPath.Add(mousePos);
         }
 
         private bool IsCreating(Vector2 mousePos)
         {
-            Vector2 lastVector = (_rootBuildingPath.RootPath[_rootBuildingPath.RootPath.Count - 1] - _rootBuildingPath.RootPath[_rootBuildingPath.RootPath.Count - 2]).normalized;
-            Vector2 newVector = (mousePos - _rootBuildingPath.RootPath[_rootBuildingPath.RootPath.Count - 1]).normalized;
+            Vector2 lastVector = (RootBuildingPath.RootPath[RootBuildingPath.RootPath.Count - 1] - RootBuildingPath.RootPath[RootBuildingPath.RootPath.Count - 2]).normalized;
+            Vector2 newVector = (mousePos - RootBuildingPath.RootPath[RootBuildingPath.RootPath.Count - 1]).normalized;
 
             float dot = Vector2.Dot(lastVector, newVector);
             if(dot < 0)
@@ -153,6 +152,14 @@ namespace Assets.Scripts.RootS
                 return false;
             }    
             return true;
+        }
+
+        /// <summary>
+        /// RootGrowthSystem.GrowRoot(_rootBuildingPath);
+        /// </summary>
+        private void CancelBluePrinting()
+        {
+            
         }
     }
 }
