9b1b765865095be5c6d5cbadd828c338acae8e0b
Anigilator322 <Anigilator322@gmail.com>
2024-12-03 21:38:09 +0700

    fix
    RootBlueprintSystem

---
+++ Assets/Scripts/RootS/PlayerRootBuilderInput.cs
@@ -0,0 +1,84 @@
+﻿using Assets.Scripts.Map;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.RootS
+{
+    public class PlayerRootBuilderInput: MonoBehaviour
+    {
+        private PlayerInputActions _playerInputActions;
+        private bool _isDragging = false;
+        private float _clickedNodeSearchRadius = 2f;
+        private GridPartition<RootNode> _gridPartition;
+        private RootBlueprintingSystem _rootBlueprintingSystem;
+        private RootNode _clickedNode;
+        private RootType _selectedType = RootType.Harvester;
+
+        void Start()
+        {
+            _playerInputActions.PlayerMap.LBMPressed.performed += _ =>
+            {
+                if (IsClickedOnRoot(_playerInputActions.PlayerMap.MousePosition.ReadValue<Vector2>()))
+                {
+                    _isDragging = true;
+                    PrepareBlueprint(_playerInputActions.PlayerMap.MousePosition.ReadValue<Vector2>());
+                }
+            };
+            _playerInputActions.PlayerMap.LBMPressed.canceled += _ => { _isDragging = false; CancelBlueprinting(); };
+        }
+
+        private void CancelBlueprinting()
+        {
+            
+        }
+
+        void Update()
+        {
+            if (!_isDragging)
+                return;
+            Vector2 mousePos = _playerInputActions.PlayerMap.MousePosition.ReadValue<Vector2>();
+            
+        }
+
+        private void DrawTrajectory(Vector2 mousePos)
+        {
+        }
+
+        private void PrepareBlueprint(Vector2 mousePosition)
+        {
+            List<RootNode> queiriedNodes = _gridPartition.Query(_clickedNodeSearchRadius, mousePosition);
+            _clickedNode = FindClosestNodeToMouse(queiriedNodes, mousePosition);
+
+            _rootBlueprintingSystem.Create(_selectedType,_clickedNode);
+        }
+
+        private bool IsClickedOnRoot(Vector2 mousePos)
+        {
+            if (_gridPartition.Query(_clickedNodeSearchRadius, mousePos).Count != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private RootNode FindClosestNodeToMouse(List<RootNode> rootNodes, Vector2 mousePosition)
+        {
+            RootNode closestNode = rootNodes[0];
+            float distance = Vector2.Distance(mousePosition, closestNode.Position);
+            foreach (RootNode node in rootNodes)
+            {
+                if (Vector2.Distance(node.Position, mousePosition) < distance)
+                {
+                    closestNode = node;
+                    distance = Vector2.Distance(node.Position, mousePosition);
+                }
+            }
+            return closestNode;
+        }
+
+    }
+}
---
+++ Assets/Scripts/RootS/RootBluePrintingSystem.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.RootS
+{
+    public class RootBlueprintingSystem
+    {
+        public float _distanceToBuildNewNode { get; private set; } = 2f;
+        public float _maxBuildAngle { get; private set; } = 90f;
+
+        private void CreateNewPathNode(RootBlueprint rootBlueprint,Vector2 targetVector, Vector2 lastNodePosition)
+        {
+            targetVector.Normalize();
+            rootBlueprint.AddInPath((lastNodePosition + targetVector) * _distanceToBuildNewNode);
+        }
+
+        private bool TryBlueprint(RootBlueprint rootBlueprint, Vector2 targetPos)
+        {
+            if (Vector2.Distance(targetPos, rootBlueprint.RootPath[^1]) <= _distanceToBuildNewNode)
+                return false;
+            if(rootBlueprint.RootPath.Count < 2)
+            {
+                CreateNewPathNode(rootBlueprint,targetPos, rootBlueprint.RootPath[^1]);
+                return true;
+            }
+            Vector2 lastPoint = rootBlueprint.RootPath[^1];
+            Vector2 secondLastPoint = rootBlueprint.RootPath[^2];
+            Vector2 directionToTarget = (targetPos - lastPoint).normalized;
+            Vector2 directionOfPath = (lastPoint - secondLastPoint).normalized;
+            
+            if (IsCreating(directionOfPath, directionToTarget))
+            {
+                float angle = Vector2.Angle(directionToTarget, directionOfPath);
+                if (angle < _maxBuildAngle)
+                {
+                    CreateNewPathNode(rootBlueprint, directionToTarget, lastPoint);
+                }
+                else
+                {
+                    Vector2 correctedPathNode = FindMaxAllowedPathNode(directionOfPath, directionToTarget);
+                    CreateNewPathNode(rootBlueprint, directionToTarget, lastPoint);
+                }
+            }
+            else
+            {
+                DecreasePath(rootBlueprint);
+            }
+            return true;
+        }
+
+        public RootBlueprint Create(RootType type, RootNode parentNode)
+        {
+            RootBlueprint rootBlueprint = new RootBlueprint(type, parentNode);
+
+            return rootBlueprint;
+        }
+
+        public RootBlueprint Update(RootBlueprint rootBlueprint, Vector2 targetPos)
+        {
+            while (TryBlueprint(rootBlueprint, targetPos)) ;
+
+            return rootBlueprint;
+        }
+
+        private Vector2 RotateVector(Vector2 v, float angle)
+        {
+            float rad = angle * Mathf.Deg2Rad;
+            float cos = Mathf.Cos(rad);
+            float sin = Mathf.Sin(rad);
+            return new Vector2(v.x * cos - v.y * sin, v.x * sin + v.y * cos);
+
+        }
+
+        private Vector2 FindMaxAllowedPathNode(Vector2 directionOfPath, Vector2 directionToTarget)
+        {
+            float sign = Mathf.Sign(Vector2.SignedAngle(directionOfPath, directionToTarget));
+            Vector2 correctedDirection = RotateVector(directionOfPath, sign * _maxBuildAngle);
+            return correctedDirection;
+        }
+
+        public float CalculateAngleBetweenNodes(Vector2 node1, Vector2 node2, Vector2 newNode)
+        {
+            
+            float cosTheta = Vector2.Dot((node2 - node1).normalized, (newNode - node2).normalized);
+            float angle = Mathf.Acos(cosTheta) * Mathf.Rad2Deg;
+            return angle;
+        }
+
+        private void DecreasePath(RootBlueprint rootBlueprint)
+        {
+            rootBlueprint.RootPath.RemoveAt(rootBlueprint.RootPath.Count-1);
+        }
+
+        private bool IsCreating(Vector2 path, Vector2 targetPos)
+        {
+            Vector2 projection = Vector2.Dot(targetPos, path) / Vector2.Dot(path, path) * path;
+            float dot = Vector2.Dot(projection, path);
+            if (dot < 0)
+            {
+                return false;
+            }    
+            return true;
+        }
+    }
+}
