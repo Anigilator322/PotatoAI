bf626a6814482f54fb838677fd376440888dc1c8
Anigilator322 <Anigilator322@gmail.com>
2024-12-10 19:04:47 +0700

    debug things

---
+++ Assets/Scripts/RootS/NodesGizmos.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Assets.Scripts.RootS.Plants;
+
+namespace Assets.Scripts.RootS
+{
+    public class NodesGizmos : MonoBehaviour
+    {
+        [SerializeField] Plant plant;
+
+        private void OnDrawGizmos()
+        {
+            if (plant.Roots.Nodes.Count > 0)
+            {
+                foreach (var node in plant.Roots.Nodes)
+                {
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawSphere(node.Position, 0.1f);
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/RootS/Plants/Plant.cs
+++ Assets/Scripts/RootS/Plants/Plant.cs
@@ -6,8 +6,15 @@ namespace Assets.Scripts.RootS.Plants
 {
     public class Plant : MonoBehaviour
     {
+        [Inject]
         public PlantRoots Roots;
-        
-        
+        [Inject]
+        GridPartition<RootNode> GridPartition; // This field used for testing purposes
+
+        private void Start()
+        {
+            Roots.Nodes.Add(new RootNode(gameObject.transform.position));
+            GridPartition.Insert(Roots.Nodes[0]);
+        }
     }
 }
