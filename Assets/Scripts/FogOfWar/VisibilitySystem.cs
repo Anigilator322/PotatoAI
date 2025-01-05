986b64680977e3b42751aabe2388f0e414a0eb91
Anigilator322 <Anigilator322@gmail.com>
2025-01-05 23:44:07 +0700

    sabj

---
+++ Assets/Scripts/FogOfWar/VisibilitySystem.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Scripts.Map;
+using Assets.Scripts.Roots;
+using UnityEngine;
+
+namespace Assets.Scripts.FogOfWar
+{
+    public class VisibilitySystem
+    {
+        private GridPartition<RootNode> _gridPartition;
+
+        private void CapsuleCast(Vector2 start, Vector2 end, float radius)
+        {
+            //Search cells in capsule
+            //Return List<Cell> with all cells in capsule
+
+        }
+
+        public void UpdateVisibilityForRootNode(RootNode revealer)
+        {
+            var edge = revealer.Position - revealer.Parent.Position;
+            int revealRadius = ((int)revealer.Type);//Make configuration for revealRadius of different rootTypes
+            float length = edge.magnitude;
+            float width = revealRadius * 2;
+            //var area = CapsuleCast()
+            // Make BFS from revealer to all cells in capsule by quadrants. From 0 to 2PI
+            // Make BFS from revealer.Parent to all cells in capsule by quadrants. From 0 to 2PI
+            // Mark all gained cells as visible
+            // Notify Cells about visibility change
+        }
+
+        public void UpdateVisibilityForResourcePoint()
+        {
+
+        }
+    }
+}
